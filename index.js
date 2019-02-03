b8e5b174aede35b01493916ac9874ba0581f318c
Jesse Florig <jesseflorig@gmail.com>
2019-02-03 16:46:05 -0500

    WIP munging

--- index.js
+++ index.js
@@ -4,4 +4,27 @@
  *
  */
 
-console.log("Hello World");
+const { map } = require("lodash");
+const { Cards } = require("netrunner-json");
+
+const mung = {};
+
+const cfg = {
+  input: Cards,
+  output: [],
+  strategies: []
+};
+
+const Mungbean = ({ input, output, strategies }) => {
+  map(input, item => {
+    let mungedItem = item;
+    map(strategies, strat => {
+      mungedItem = strat(item);
+    });
+    output.push(mungedItem);
+  });
+
+  console.log(output[0]);
+};
+
+Mungbean(cfg);
