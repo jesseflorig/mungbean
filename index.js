a7080efff425a9ebe048b946992dd0546303f498
Jesse Florig <jesseflorig@gmail.com>
2019-04-28 21:47:27 -0400

    updated format

--- index.js
+++ index.js
@@ -4,26 +4,32 @@
  */
 
 const { map } = require("lodash");
-const Cards = require("../netrunner-json/cards.json");
-const { Mungbean, addField, capVal, genVal } = require("./mungbean");
+const { Mungbean, addField, chgKey, genVal, lowerVal } = require("./mungbean");
+const { getAllCards } = require("./netrunnerUtils");
 
-console.log("cards", Cards.length);
+const cards = getAllCards();
+
 const newId = genVal({
-  fields: ["pack", "position"],
+  fields: ["pack_code", "position"],
   delimiter: "-"
 });
 
 const cfg = {
-  input: Cards,
-  output: [],
+  env: "prod",
+  input: cards,
+  output: "../netrunner-json/cards.json",
   strategies: [
     addField("id", newId),
-    addField("test", "test"),
-    capVal("faction"),
-    capVal("side"),
-    capVal("type")
+    chgKey("faction_code", "faction"),
+    chgKey("faction_cost", "influence"),
+    chgKey("deck_limit", "limit"),
+    chgKey("side_code", "side"),
+    chgKey("pack_code", "pack"),
+    chgKey("type_code", "type"),
+    chgKey("quantity", "qty"),
+    chgKey("uniqueness", "unique"),
+    lowerVal("keywords")
   ]
 };
 
-const munged = Mungbean(cfg);
-console.log(munged[0]);
+Mungbean(cfg);
--- mungbean.js
+++ mungbean.js
@@ -2,164 +2,172 @@
 //   field - a key/value pair
 //   key - reference for a value
 //   value - contents of a key
-const fs = require('fs')
-const { map } = require('lodash')
-const { dev, err, hr, log, plur } = require('./utils')
+const fs = require("fs");
+const { map } = require("lodash");
+const { dev, err, hr, log, plur } = require("./utils");
 
 const defaultCfg = {
-  env: 'dev',
+  env: "dev",
   input: null,
-  output: './output'
-}
+  output: "./output"
+};
 
 // Apply a strategy to an input to create an output
 const Mungbean = config => {
   // Merge default config with passed in config
-  const cfg = Object.assign(defaultCfg, config)
-  const { env, input, output, strategies } = cfg
+  const cfg = Object.assign(defaultCfg, config);
+  const { env, input, output, strategies } = cfg;
 
-  dev('Running DEV munge', env)
+  dev("Running DEV munge", env);
 
   // Munge processor
-  let results = []
+  let results = [];
   map(input, item => {
-    let mungedItem = flow(strategies)(item)
-    results.push(mungedItem)
-  })
+    let mungedItem = flow(strategies)(item);
+    results.push(mungedItem);
+  });
 
   // Finish
-  hr()
-  const inCount = input.length
-  log(`Finished processing ${inCount} ${plur('record', inCount)}`)
-  dev(results[0], env)
-  if (env === 'prod') {
-    writeFile(output, results)
+  hr();
+  const inCount = input.length;
+  log(`Finished processing ${inCount} ${plur("record", inCount)}`);
+  dev(results[0], env);
+  if (env === "prod") {
+    writeFile(output, results);
   }
-}
+};
 
 const flow = fns => item => {
   map(fns, fn => {
-    item = fn(item)
-  })
-  return item
-}
+    item = fn(item);
+  });
+  return item;
+};
 
 const writeFile = (path, data) => {
-  log(`Writing file to ${path}`)
-  const jsonData = JSON.stringify(data, null, 2)
+  log(`Writing file to ${path}`);
+  const jsonData = JSON.stringify(data, null, 2);
 
   fs.writeFile(path, jsonData, err => {
     if (err) {
-      err(err)
+      err(err);
     }
-    log(`Finished writing file to ${path}`)
-  })
-}
+    log(`Finished writing file to ${path}`);
+  });
+};
 
 // CSV to JSON
 const csvToJson = path => {
-  require('require-csv')
-  const csv = require(path)
-  const keys = csv[0]
-  let results = []
+  require("require-csv");
+  const csv = require(path);
+  const keys = csv[0];
+  let results = [];
 
   map(csv, (line, idx) => {
     if (idx) {
-      let item = {}
+      let item = {};
       map(keys, (key, kIdx) => {
-        item[key] = line[kIdx]
-      })
-      results.push(item)
+        item[key] = line[kIdx];
+      });
+      results.push(item);
     }
-  })
-  log(`Converted CSV to ${csv.length - 1} JSON items`)
-  return results
-}
+  });
+  log(`Converted CSV to ${csv.length - 1} JSON items`);
+  return results;
+};
 
 // Add a new field
 const addField = (newKey, val) => item => {
-  let newItem = item
+  let newItem = item;
   if (item[newKey]) {
-    err(`Field '${newKey}' already exists - skipping add`)
+    err(`Field '${newKey}' already exists - skipping add`);
   } else {
-    newItem[newKey] = typeof val === 'function' ? val(item) : val
+    newItem[newKey] = typeof val === "function" ? val(item) : val;
   }
-  return newItem
-}
+  return newItem;
+};
+
+// Generate a value from one or more existing fields
+const genVal = ({ fields, delimiter, prefix, postfix }) => item => {
+  let fieldVals = map(fields, f => item[f]);
+  fieldVals = prefix ? [prefix, ...fieldVals] : fieldVals;
+  fieldVals = postfix ? [...fieldVals, postfix] : fieldVals;
+  return fieldVals.join(delimiter);
+};
 
 // Remove a field
 const delField = key => item => {
-  let newItem = item
+  let newItem = item;
   if (item[key] !== undefined) {
-    delete newItem[key]
+    delete newItem[key];
   } else {
-    err(`Field '${key}' doesn't exist - skipping delete`)
+    // err(`Field '${key}' doesn't exist - skipping delete`);
   }
-  return newItem
-}
+  return newItem;
+};
 
 // Change a field key
 const chgKey = (oldKey, newKey) => item => {
-  return flow([addField(newKey, item[oldKey]), delField(oldKey)])(item)
-}
+  return flow([addField(newKey, item[oldKey]), delField(oldKey)])(item);
+};
 
 // Generate a value from one or more existing fields
 const concatVals = ({ fields, delimiter, prefix, postfix }) => item => {
-  let fieldVals = map(fields, f => item[f])
-  fieldVals = prefix ? [prefix, ...fieldVals] : fieldVals
-  fieldVals = postfix ? [...fieldVals, postfix] : fieldVals
-  return fieldVals.join(delimiter)
-}
+  let fieldVals = map(fields, f => item[f]);
+  fieldVals = prefix ? [prefix, ...fieldVals] : fieldVals;
+  fieldVals = postfix ? [...fieldVals, postfix] : fieldVals;
+  return fieldVals.join(delimiter);
+};
 
 // Apply a function to a field if it exists
 const modField = fn => field => item => {
   if (item[field]) {
-    const newVal = fn(item[field])
-    item[field] = newVal
+    const newVal = fn(item[field]);
+    item[field] = newVal;
   }
-  return item
-}
+  return item;
+};
 
 // Uppercase a field value
 const upperVal = field => {
   return modField(input => {
-    return input.toUpperCase()
-  })(field)
-}
+    return input.toUpperCase();
+  })(field);
+};
 
 // Lowercase a field value
 const lowerVal = field => {
   return modField(input => {
-    return input.toLowerCase()
-  })(field)
-}
+    return input.toLowerCase();
+  })(field);
+};
 
 // Capitalize a field value
 const capVal = field => {
   return modField(input => {
-    return input.toLowerCase().replace(/\b\w/g, l => l.toUpperCase())
-  })(field)
-}
+    return input.toLowerCase().replace(/\b\w/g, l => l.toUpperCase());
+  })(field);
+};
 
 // Convert value to JSON date
 const cvtDate = field => item => {
-  const date = new Date(item[field])
-  item[field] = date.toJSON()
-  return item
-}
+  const date = new Date(item[field]);
+  item[field] = date.toJSON();
+  return item;
+};
 
 // Convert value to integer
-const intVal = field => item => modField(parseInt)(field)(item)
+const intVal = field => item => modField(parseInt)(field)(item);
 
 // Convert value to float
-const floatVal = field => item => modField(parseFloat)(field)(item)
+const floatVal = field => item => modField(parseFloat)(field)(item);
 
 // Convert value to dollar
 const dollarVal = field => {
   return modField(input => {
-    return parseFloat(parseFloat(input).toFixed(2))
-  })(field)
-}
+    return parseFloat(parseFloat(input).toFixed(2));
+  })(field);
+};
 
 module.exports = {
   Mungbean,
@@ -172,8 +180,9 @@ module.exports = {
   delField,
   dollarVal,
   floatVal,
+  genVal,
   intVal,
   lowerVal,
   modField,
   upperVal
-}
+};
